a43fa86d232ff0def97bd99a11d3167f0f44c660
EddyShimwa <shedpray16@gmail.com>
2023-11-08 23:46:17 +0200

    add icons on bottom nav bar

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import * as React from 'react'; 
 import { Text, View } from 'react-native'; 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 
 const Tab = createBottomTabNavigator();
 
@@ -51,18 +53,52 @@ function Favorites() {
 } 
 // const Drawer = createDrawerNavigator(); 
   
-const App = () => { 
-  return ( 
-    <NavigationContainer> 
-      <Tab.Navigator initialRouteName="Home"> 
-        <Tab.Screen name="Home" component={HomeScreen} /> 
-        <Tab.Screen name="Favz" component={Favorites} /> 
-        <Tab.Screen name="Notifications" 
-                       component={NotificationsScreen} /> 
-        <Tab.Screen name="About" component={AboutScreen} /> 
-      </Tab.Navigator> 
-    </NavigationContainer> 
-  ); 
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Home">
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="home" size={size} color={color} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Favz"
+          component={Favorites}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="heart" size={size} color={color} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="Notifications"
+          component={NotificationsScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="bell" size={size} color={color} />
+            ),
+          }}
+        />
+
+        <Tab.Screen
+          name="About"
+          component={AboutScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <Icon name="info" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App;
