828c155a30a77738b1bd9703bff4c705ddb150ae
EddyShimwa <shedpray16@gmail.com>
2023-11-17 16:53:35 +0200

    remove unwanted comments

--- src/Screens/AboutScreen.js
+++ src/Screens/AboutScreen.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Text, View } from "react-native";
-import { ScrollView} from "react-native"
 const AboutScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
