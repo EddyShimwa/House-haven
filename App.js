aa6c4716e6975844566d73fb4f34befaeb9c97be
EddyShimwa <shedpray16@gmail.com>
2023-11-17 16:51:40 +0200

    remove about stack

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { Text, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { NavigationContainer } from "@react-navigation/native";
