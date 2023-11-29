ee17a1c4b8d1fc58c3c139000e21b2f460150158
EddyShimwa <shedpray16@gmail.com>
2023-11-29 15:51:35 +0200

    work on favorites pages

--- src/Screens/FavoritesScreen.js
+++ src/Screens/FavoritesScreen.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 const Favorites = () => {
   return (
+    <>
+    <View>
+     <Text style={{marginTop: 50, fontSize: 30, display: 'flex', textAlign: 'center'}}>My Favorites</Text>
+     </View>
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Favorites Page</Text>
+      <Text>Favorites Page</Text>marginTop: 50,
     </View>
+    </>
   );
 };
 export default Favorites;
